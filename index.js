2f4fc964725a0ee23773bb0371bb495ebdb6c538
Hannah Shine <hshine@visitationacademy.org>
2018-06-21 21:06:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue){
+  if (someValue<=400){
+    return "This one is on me!";
+    }
+  else if (someValue>2000 && someValue<2500) {
+    return "I will gladly take your thirty bucks."
+    }
+  else if(someValue>=2500){
+    return "No can do."
+  }
+}
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+}
+var text;
+var switchOnCharmFromTip = document.getElementById("generous").value;
+switch("generous"){
+  case:"generous";
+  text="Thank you so much.";
+  case:"not as generous";
+  text="Thank you.";
+  
+  
+}
